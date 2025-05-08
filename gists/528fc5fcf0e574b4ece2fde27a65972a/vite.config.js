6cdcc1c64691ad87f3f95fe3bf30b9ba86a08c81
rim <18486012+0RIM0@users.noreply.github.com>
2025-05-09 00:03:22 +0900

    デプロイ先に合わせてビルド設定

---
+++ gists/528fc5fcf0e574b4ece2fde27a65972a/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+	base: "/gist-resources/gists/528fc5fcf0e574b4ece2fde27a65972a/",
+	build: {
+		outDir: "_pages_",
+	},
+})
