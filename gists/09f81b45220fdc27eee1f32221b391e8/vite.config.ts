4fc208b6558d3d592e891d4ec9eec6b0ce4c591e
rim <18486012+0RIM0@users.noreply.github.com>
2026-02-25 23:13:50 +0900

    公開時のベースパス修正

--- gists/09f81b45220fdc27eee1f32221b391e8/vite.config.ts
+++ gists/09f81b45220fdc27eee1f32221b391e8/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "./",
+  build: {
+    outDir: "_pages_",
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
